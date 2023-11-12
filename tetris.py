4665156a988b92711ad8a70c4766ef0111996c52
Lucien Fiorini <lucien.fiorini@student.uclouvain.be>
2023-11-12 03:25:44 +0100

    epic commit

--- tetris.py
+++ tetris.py
@@ -2,42 +2,35 @@ import numpy as np
 import pygame as pg
 import random
 import graphics
+import time
 
-pieces = [
+PIECES = [
     [
         [0,0,1],
         [1,1,1],
-        [0,0,0]
     ],
     [
         [1,0,0],
         [1,1,1],
-        [0,0,0]
     ],
     [
         [0,1,1],
         [1,1,0],
-        [0,0,0]
     ],
     [
         [1,1,0],
         [0,1,1],
-        [0,0,0]
     ],
     [
         [0,1,0],
         [1,1,1],
-        [0,0,0]
     ],
     [
         [1,1],
         [1,1]
     ],
     [
-        [0,0,0,0],
         [1,1,1,1],
-        [0,0,0,0],
-        [0,0,0,0]
     ]
 ]
 
@@ -49,10 +42,10 @@ class Tetris:
         self.current_piece = 0
         self.next_piece = 0
 
-    def place_pieces(self, piece, size, x, y):
-        for i in range(size):
-            for j in range(size):
-                if y + j < 22 and x + i < 12 and self.board[y + j][x + i] == 0 and piece[j][i] != 0:
+    def place_pieces(self, piece, size_x, size_y, x, y):
+        for i in range(size_x):
+            for j in range(size_y):
+                if x + i < 12 and self.board[y + j][x + i] == 0 and piece[j][i] != 0:
                     self.board[y + j][x + i] = piece[j][i] * self.current_piece
 
     def check_lines(self):
@@ -81,34 +74,27 @@ class Tetris:
             self.current_piece = random.randint(1, 7)
         else:
             self.current_piece = self.next_piece
-
                 
     def play(self, rot, col):
-        piece = np.rot90(pieces[self.current_piece - 1], rot)
-        size = len(pieces[self.current_piece - 1])
+        piece = np.rot90(PIECES[self.current_piece - 1], rot)
+        size_y, size_x = piece.shape
 
-        if col + size > 12:
-            if np.any(piece[:, size - 1] != 0):
-                self.game_over = True
-                return
-
-            # dirty hack for I piece
-            if self.current_piece == 7 and np.any(piece[:, size - 2] != 0):
-                self.game_over = True
-                return
+        # Quick out of bound check
+        if col + size_x > 12:
+            self.game_over = True
+            return
 
         # Place the piece
-        for y in range(0, 23 - size):
-            if y == 22 - size:
-                self.place_pieces(piece, size, col, y)
+        for y in range(0, 23 - size_y):
+            if y == 22 - size_y:
+                self.place_pieces(piece, size_x, size_y, col, y)
                 self.check_lines()
                 return
 
-            for i in range(size):
-                for j in range(size):
+            for i in range(size_x):
+                for j in range(size_y):
                     if col + i < 12 and piece[j][i] != 0 and self.board[j + y + 1][i + col] != 0:
-
-                        self.place_pieces(piece, size, col, y)
+                        self.place_pieces(piece, size_x, size_y, col, y)
 
                         if np.any(self.board[0] != 0):
                             self.game_over = True
@@ -121,16 +107,24 @@ class Tetris:
         print(self.board)
 
 if __name__ == "__main__":
+
     while True:
         t = Tetris()
+        graphic = graphics.Graphic(300, (64, 201, 255), (232, 28, 255), (255, 255, 255), t.board)
 
         while not t.game_over:
             t.next_pos()
-            t.play(random.randint(0, 2), random.randint(0, 11))
+            t.current_piece = 7
+            t.next_piece = 7
+            t.play(0, random.randint(0, 9))
             t.print()
+            graphic.draw()
+            time.sleep(0.1)
 
     # graphic = graphics.Graphic(300, (64, 201, 255), (232, 28, 255), (255, 255, 255), t.board)
     # graphic.draw()
+    # pg.quit()
+    # quit()
     # while True:
     #      for event in pg.event.get():
     #          if event.type == pg.QUIT:
