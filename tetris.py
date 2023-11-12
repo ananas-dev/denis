c4027cdb93a8e9c012fbfb5bb71ae353a96fec0c
Lucien Fiorini <lucien.fiorini@student.uclouvain.be>
2023-11-12 07:49:21 +0100

    more inputs

--- tetris.py
+++ tetris.py
@@ -2,6 +2,25 @@ import numpy as np
 import random
 import time
 
+# Antoine's special
+def getAmas(matrix, x, y):
+    target_num = matrix[y][x]
+    rows = len(matrix)
+    cols = len(matrix[0])
+    visited = [[False] * cols for _ in range(rows)]
+    
+    def dfs(x, y):
+        if x < 0 or x >= cols or y < 0 or y >= rows or visited[y][x] or matrix[y][x] != target_num:
+            return []
+        visited[y][x] = True
+        coordinates = [(x, y)]
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            next_x, next_y = x + dx, y + dy
+            coordinates += dfs(next_x, next_y)
+        return coordinates
+
+    return dfs(x, y)
+
 PIECES = [
     [
         [0,0,1],
@@ -37,10 +56,12 @@ class Tetris:
             self,
             board=np.zeros((22, 12)),
             score = 0,
+            cleared = 0,
             current_piece=random.randint(1, 7),
             next_piece=random.randint(1, 7)):
         self.board = board
         self.score = score
+        self.cleared = cleared
         self.current_piece = current_piece
         self.next_piece = next_piece
 
@@ -56,7 +77,6 @@ class Tetris:
     
     def place_pieces(self, piece, size_x, size_y, x, y):
         new_board = self.board.copy()
-        new_score = self.score
 
         for i in range(size_x):
             for j in range(size_y):
@@ -72,8 +92,7 @@ class Tetris:
                 clear_count += 1
 
 
-        new_score += clear_count * 1000
-        return new_board, new_score
+        return new_board, self.score + clear_count * 1000, self.cleared + clear_count
 
     def height_multiplier(self):
         res = 0
@@ -85,7 +104,22 @@ class Tetris:
         return res
 
     def holes(self):
-        res = 0
+        formes = []
+        already_visited = []
+
+        for y, row in enumerate(self.board):
+            for x, elem in enumerate(row):
+            
+                # Nouvelle pièce à ajouter à la collection
+                if elem == 0 and (x, y) not in already_visited:
+                    w = getAmas(self.board, x, y)  # Forme W enregistrée.
+                    # On voudrait éviter de refaire un traitement pour une case
+                    # qui appartient à W, parce que ça ferait exactement la même forme.
+                    for coord in w:
+                        already_visited.append(coord)
+                    formes.append(w)
+        
+        return (len(formes) - 1)
 
     def blocades(self):
         res = 0
@@ -104,18 +138,18 @@ class Tetris:
 
         for y in range(0, 23 - size_y):
             if y == 22 - size_y:
-                new_board, new_score = self.place_pieces(piece, size_x, size_y, col, y)
-                return False, Tetris(new_board, new_score, self.next_piece, next_piece)
+                new_board, new_score, new_cleared = self.place_pieces(piece, size_x, size_y, col, y)
+                return False, Tetris(new_board, new_score, new_cleared, self.next_piece, next_piece)
 
             for i in range(size_x):
                 for j in range(size_y):
                     if col + i < 12 and piece[j][i] != 0 and self.board[j + y + 1][i + col] != 0:
-                        new_board, new_score = self.place_pieces(piece, size_x, size_y, col, y)
+                        new_board, new_score, new_cleared = self.place_pieces(piece, size_x, size_y, col, y)
 
                         if np.any(new_board[0] != 0):
                             return True, None
 
-                        return False, Tetris(new_board, new_score, self.next_piece, next_piece)
+                        return False, Tetris(new_board, new_score, new_cleared, self.next_piece, next_piece)
                 
     def print(self):
         print(self.board)
@@ -131,7 +165,7 @@ if __name__ == "__main__":
             exit()
         for rot1, col1 in t1.gen_legal_moves():
             game_over, t2 = t1.apply_move(rot1, col1, leaf=True)
-            t2.height_muliplier()
+            t2.height_multiplier()
             if game_over:
                 exit()
             # t2.print()
