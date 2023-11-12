c8467eac533174c074929d5a4ef4431d323cafb9
Antoine Berthion <128331631+Ant0in@users.noreply.github.com>
2023-11-12 05:27:32 +0100

    bahagbuhiaduhzd
    lucas ntm si le format change ptdr

--- mainLoop.py
+++ mainLoop.py
@@ -1,6 +1,16 @@
 from image_recognition import getData
 import numpy as np
+import pyautogui
+import random
 
+def playMove(move, rotate: int):
+    if move > 0:
+        for _ in range(move): pyautogui.press('right')
+    elif move < 0:
+        for _ in range(move * -1): pyautogui.press('left')
+    else: pass
+    for _ in range(rotate): pyautogui.press('up')
+    pyautogui.press('space')
 
 def main():
 
@@ -21,10 +31,14 @@ def main():
         if not np.all(difference == 0):
             # On doit jouer un coup avec les data de Frame.
             print(f"On doit jouer un coup !")
+            playMove(random.randint(-5, 5), random.randint(0, 3))
             ...  # Get bests moves from AI.
             ...  # Some function 2 execute inputs.
 
         prev_iteration = iteration  # On update l'ancienne itération. >___<"
 
 
-main()
+if __name__ == '__main__':
+    # Faster inputs (.01 delay is fine it seems)
+    pyautogui.PAUSE = 0.01
+    main()  # Main execution
